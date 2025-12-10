34544d810afd7c9d91da27377e9b9ea68fefa8a0
RR <rrn091301@gmail.com>
2025-12-10 15:19:34 +0800

    refactor: extract empty cart component into a reusable results component

---
+++ src/components/Results.jsx
@@ -0,0 +1,18 @@
+import styles from './Results.module.css'
+
+function Results({ mainHeading, bottomText, children }) {
+  return (
+    <div>
+      <div className={styles.resultsMain}>
+        <h1 className={styles.resultsMainHeading}>{mainHeading}</h1>
+        {children}
+        <div className={styles.resultsMainBackground}></div>
+      </div>
+      <div className={styles.resultsBottom}>
+        <p>{bottomText}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Results
--- src/routes/cart_route/CartRoute.jsx
+++ src/routes/cart_route/CartRoute.jsx
@@ -2,34 +2,29 @@ import styles from './CartRoute.module.css'
 import { useLoaderData, Link } from 'react-router-dom'
 import CartItems from './CartItems'
 import OrderSummary from './OrderSummary'
+import Results from '../../components/Results'
 
 function CartRoute() {
   const [orderSummary, cartItems] = useLoaderData()
   const isCartEmpty = cartItems.length === 0
 
-  if (isCartEmpty) return <EmptyCart />
+  if (isCartEmpty) {
+    return (
+      <Results
+        mainHeading='Your cart is empty'
+        bottomText='Did you forget to buy items before heading into lane again?'
+      >
+        <div className={styles.shopNowWrapper}>
+          <Link className={styles.shopNow} to={'/shop'} aria-label="shop now" role="button">Shop Now</Link>
+        </div>
+      </Results>
+    )
+  }
 
   return (
     <div className={styles.cartRouteWrapper}>
       <CartItems cartItems={cartItems} />
       <OrderSummary orderSummary={orderSummary} />
-    </div>
-  )
-}
-
-function EmptyCart() {
-  return (
-    <div>
-      <div className={styles.emptyCartMain}>
-        <h1 className={styles.emptyCartMainHeading}>Your cart is empty</h1>
-        <div className={styles.shopNowWrapper}>
-          <Link className={styles.shopNow} to={'/shop'} aria-label="shop now" role="button">Shop Now</Link>
-        </div>
-        <div className={styles.emptyCartMainBackground}></div>
-      </div>
-      <div className={styles.emptyCartBottom}>
-        <p>Did you forget to buy items before heading into lane again?</p>
-      </div>
     </div>
   )
 }
